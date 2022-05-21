feb997aa214f2c91d07b72e5fb502b5383207c01
cobeos <cobeos@outlook.com>
2022-05-20 19:19:44 -0500

    first commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -48,7 +48,7 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
-  delete objeto[unapropiedad];
+  delete objeto[unaPropiedad];
   
   return objeto;
 }
