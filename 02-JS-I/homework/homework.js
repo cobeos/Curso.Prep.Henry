414a2f18e2c84172dd8cad8ec3750704da1b0788
cobeos <cobeos@outlook.com>
2022-06-26 15:11:18 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 9;
+const nuevoNum = 7;
 
 // Crea una variable booleana:
 const nuevoBool = true;
@@ -29,7 +29,7 @@ function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
   return str;
-  
+    
 }
 
 function suma(x, y) {
--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -84,7 +84,9 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  return numero === 10 || numero === 5;
+  if(numero == 10 || numero == 5)
+  return true;
+  else{ return false;}
 }
 
 function estaEnRango(numero) {
